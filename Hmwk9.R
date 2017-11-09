0cd1318315aa32a771ba0b1f11f35d8eff850570
almccombs <amccombs@iastate.edu>
2017-11-08 21:05:21 -0600

    half of problem 5

--- Hmwk9.R
+++ Hmwk9.R
@@ -61,3 +61,50 @@ order.matrix(mymat)
 
 mymat <- matrix(rchisq(20, 1), nrow = 5)
 order.matrix(mymat)
+
+rm(list = ls())
+
+#Problem 5
+
+  #Problem 5.a
+polaroid <- function(x) {
+  p <- length(x)
+  r <- sqrt(sum(x^2))
+
+  theta <- rep(0, p-1)
+  den <- rep(0, p-2)
+
+  theta[1] <- acos(x[1]/r)
+  den[1] <- r
+
+  for (i in 2:(p-1)) {
+    den[i] <- den[i-1] * sin(theta[i-1])
+    theta[i] <- acos(x[i]/den[i])
+  }
+
+  polar <- c(r, theta)
+  return(polar)
+}
+
+x <- seq(from = 0, to = 10, by = 2)
+polaroid(x)
+
+  #Problem 5.b
+normalize <- function(mymat) {
+  divisor <- rowSums(mymat)
+  for (r in 1:nrow(mymat)) {
+    for (c in 1:ncol(mymat)) {
+      mymat[r,c] <- mymat[r,c]/divisor[r]
+    }
+  }
+  return(mymat)
+}
+
+mymat <- matrix(5:13, nrow = 3)
+normalize(mymat)
+
+  #Problem 5.c
+
+
+
+
